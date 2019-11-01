3233ac2d4285e14151e6c4920f69fd6634d1b0df
twells38 <35282627+twells38@users.noreply.github.com>
2019-11-01 15:37:12 -0400

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class DoWhile
+{
+   public static void main(String[]args)
+   { 
+       Scanner input = new Scanner(System.in);
+       System.out.print("Please enter count number: ");
+       int count = input.nextInt();
+
+       do{
+       System.out.print("Hello! ");
+       count = count -1;
+       }
+       while(count>0);
+   }
+}
