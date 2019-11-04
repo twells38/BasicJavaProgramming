4fa56639604b63be2ecd901e6f152847a73dc14c
twells38 <35282627+twells38@users.noreply.github.com>
2019-11-04 15:48:57 -0500

    Add files via upload

---
+++ FindMaxNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FindMaxNumber
+{
+   public static void main(String[]args){
+   int i;
+   System.out.print("Enter number of elements in array: ");
+    Scanner input = new Scanner(System.in);
+    int num = input.nextInt();
+    System.out.print("Enter elements of array: ");
+    int[]arr = new int[num];
+        for(i=0; i< num;i++){
+       arr[i] = input.nextInt();
+      } 
+     int max= arr[0];
+     for(i=0; i<num; i++){
+    if ( arr[i] > max){
+      max = arr[i];}
+      }
+      System.out.println("Largest number in array is:"+max);
+     
+}
+}
