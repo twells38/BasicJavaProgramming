887b29781491bf157495c22ddc39ede55e8046bd
twells38 <35282627+twells38@users.noreply.github.com>
2020-02-15 00:20:11 -0500

    Add files via upload

---
+++ TriangleNumbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class TriangleNumbers
+{
+     public static void main(String[]args)
+    {
+      int num;
+      System.out.print("Please enter number: ");
+      Scanner input = new Scanner(System.in);
+              num = input.nextInt();
+      printTriangleNumbers(num);
+     }
+     
+     public static void printTriangleNumbers(int number)
+     {
+        int limit = number;
+        
+        for(int i=0; i < limit;i++){
+          for(int j =1; j<=number;j++){
+          System.out.print(j+" ");
+                    
+         }
+         System.out.println();
+         number--;
+
+         }
+     }
+         
+}
