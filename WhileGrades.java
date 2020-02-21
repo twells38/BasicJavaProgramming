e19a9aab98a78d990c7da7db89038941e600eeb2
twells38 <35282627+twells38@users.noreply.github.com>
2020-02-20 21:30:47 -0500

    Add files via upload

---
+++ WhileGrades.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class WhileGrades
+{
+    public static void main(String[]args)
+    {    
+         int count=1; // initialized count to get in while loop
+         double average;
+         int sum=0;
+         Scanner input = new Scanner(System.in);
+                  
+         while(count<=3){
+         System.out.print("Please enter grade"+count+" 1 to 100: ");
+         int grade = input.nextInt();
+         if(grade<0 || grade>100){
+         System.out.println("ERROR!");
+         }else{
+         sum += grade;
+         count++;
+         }
+        }
+         average = sum/(count-1);
+         System.out.println("The average of your grades is "+average);
+         if(average >=90){
+           System.out.println("YOU GOT A");
+          }else 
+          if(average>=80){
+          System.out.println("YOU GOT B");
+          }else 
+          if(average >= 70){
+          System.out.println("YOU GOT C");
+          } 
+          else 
+          if(average >=60){
+          System.out.println("YOU GOT D");
+          }
+          else{
+          System.out.println("YOU GOT F");
+         
+         
+        
+     }
+    }
+   }
